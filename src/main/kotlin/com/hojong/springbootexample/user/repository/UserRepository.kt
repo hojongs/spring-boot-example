c4ed1c878832747c909591063a9cea67fc81e608
Jongho Jeon <hojong.jjh@gmail.com>
2021-03-13 21:05:03 +0900

    Add findByName and test cases

--- src/main/kotlin/com/hojong/springbootexample/user/repository/UserRepository.kt
+++ src/main/kotlin/com/hojong/springbootexample/user/repository/UserRepository.kt
@@ -3,4 +3,6 @@ package com.hojong.springbootexample.user.repository
 import com.hojong.springbootexample.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Long>
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByName(name: String): User?
+}
--- src/test/kotlin/com/hojong/springbootexample/user/repository/UserRepositoryTest.kt
+++ src/test/kotlin/com/hojong/springbootexample/user/repository/UserRepositoryTest.kt
@@ -2,8 +2,13 @@ package com.hojong.springbootexample.user.repository
 
 import com.hojong.springbootexample.user.entity.User
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.dao.IncorrectResultSizeDataAccessException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestConstructor
@@ -14,18 +19,70 @@ import org.springframework.test.context.TestConstructor
 class UserRepositoryTest(
     private val userRepository: UserRepository,
 ) {
+    private lateinit var savedTestUser: User
 
-    @Test
-    fun save() {
-        // when
-        val user = User.of("hojong")
-        val saved = userRepository.save(user)
+    @BeforeEach
+    fun setUp() {
+        val user = User.of("test-user")
+        savedTestUser = userRepository.save(user)
+    }
 
-        // then
-        val found = userRepository.findByIdOrNull(saved.id)!!
-        println(found)
+    @AfterEach
+    fun tearDown() {
+        userRepository.deleteAll()
+    }
 
-        assertThat(saved.id)
-            .isNotEqualTo(0)
+    @Nested
+    inner class Save {
+        @Test
+        fun success() {
+            // when
+            val user = User.of("hojong")
+            val saved = userRepository.save(user)
+
+            // then
+            val found = userRepository.findByIdOrNull(saved.id)!!
+            println(found)
+
+            assertThat(saved.id)
+                .isNotEqualTo(0)
+        }
+    }
+
+    @Nested
+    inner class FindByName {
+        @Test
+        fun `given there is single user matched name, then return the user`() {
+            // when
+            val found = userRepository.findByName(savedTestUser.name)
+
+            // then
+            println(found)
+            assertThat(found)
+                .isEqualTo(savedTestUser)
+        }
+
+        @Test
+        fun `given there isn't a user matched name, then return null`() {
+            // when
+            val found = userRepository.findByName("null")
+
+            // then
+            println(found)
+            assertThat(found)
+                .isNull()
+        }
+
+        @Test
+        fun `given there are 2 users matched name, then error`() {
+            // given
+            val user = User.of(savedTestUser.name)
+            savedTestUser = userRepository.save(user)
+
+            // when
+            assertThrows<IncorrectResultSizeDataAccessException> {
+                userRepository.findByName(savedTestUser.name)
+            }
+        }
     }
 }
